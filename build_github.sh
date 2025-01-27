131a22e7570e12409d4369e0adbc156a5105b3e6
Keith Perfetti <perfetti@hbnetworks.net>
2025-01-27 10:53:00 -0500

    Trying out running from docker-compose directly

--- build_github.sh
+++ build_github.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-if [ ! -f .env ]; then
-  export FRONTEND_BUILD_CONTEXT="https://${GH_TOKEN}@${FRONTEND_REPO_URL}#${FRONTEND_REF}"
-  export API_BUILD_CONTEXT="https://${GH_TOKEN}@${API_REPO_URL}#${API_REF}"
-  export BACKEND_BUILD_CONTEXT="https://${GH_TOKEN}@${BACKEND_REPO_URL}#${BACKEND_REF}"
-
-  echo "" >> .env
-  echo "FRONTEND_BUILD_CONTEXT='${FRONTEND_BUILD_CONTEXT}'" >> .env
-  echo "API_BUILD_CONTEXT='${API_BUILD_CONTEXT}'" >> .env
-  echo "BACKEND_BUILD_CONTEXT='${BACKEND_BUILD_CONTEXT}'" >> .env
-fi
-
-source .env
+./setup_github.sh
 
 # Run Docker Compose without local volumes
 docker-compose up --build --no-deps -d
