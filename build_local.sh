95c7d177e0be0ff312cbce19d88d02d4835bb7ec
Keith Perfetti <perfetti@hbnetworks.net>
2025-02-03 14:35:22 -0500

    Fixing local system build

--- build_local.sh
+++ build_local.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-source ./setup_local.sh
+source .env
+
+# Set environment variables for local build
+export FRONTEND_BUILD_CONTEXT=./repo_refs/react1-app
+export API_BUILD_CONTEXT=./repo_refs/rails-app
+export BACKEND_BUILD_CONTEXT=./repo_refs/react2-app
+
+echo $FRONTEND_BUILD_CONTEXT
+echo $API_BUILD_CONTEXT
+echo $BACKEND_BUILD_CONTEXT
 
 # Run Docker Compose
 docker-compose --profile core -f docker-compose.local.yaml up --build -d
