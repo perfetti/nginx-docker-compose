6a7fc04519919bd7a4a66b17ccd311ad237e3059
Keith Perfetti <perfetti@hbnetworks.net>
2025-07-23 19:02:46 -0400

    We are attached

--- bin/build_github.sh
+++ bin/build_github.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-if [ ! -f .env ]; then
-  export FRONTEND_BUILD_CONTEXT="https://${GH_TOKEN}@${FRONTEND_REPO_URL}#${FRONTEND_REF}"
-  export API_BUILD_CONTEXT="https://${GH_TOKEN}@${API_REPO_URL}#${API_REF}"
-  export BACKEND_BUILD_CONTEXT="https://${GH_TOKEN}@${BACKEND_REPO_URL}#${BACKEND_REF}"
-
-  echo "" >> .env
-  echo "FRONTEND_BUILD_CONTEXT='${FRONTEND_BUILD_CONTEXT}'" >> .env
-  echo "API_BUILD_CONTEXT='${API_BUILD_CONTEXT}'" >> .env
-  echo "BACKEND_BUILD_CONTEXT='${BACKEND_BUILD_CONTEXT}'" >> .env
-fi
 
 source .env
 
@@ -25,4 +15,4 @@ docker compose exec nginx nginx -s reload
 docker rm nginx_tmp
 
 # Run Docker Compose without local volumes
-docker-compose --profile core up --build --no-deps -d
+docker-compose --profile core up --build --no-deps
