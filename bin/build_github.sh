6a3a0e1da06d257abee8e2c11452cf5d5b9528b3
Keith Perfetti <perfetti@hbnetworks.net>
2025-07-23 19:50:49 -0400

    fixing build_github

--- bin/build_github.sh
+++ bin/build_github.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#  We need this for proper circleci builds.
 if [ ! -f .env ]; then
   export FRONTEND_BUILD_CONTEXT="https://${GH_TOKEN}@${FRONTEND_REPO_URL}#${FRONTEND_REF}"
   export API_BUILD_CONTEXT="https://${GH_TOKEN}@${API_REPO_URL}#${API_REF}"
